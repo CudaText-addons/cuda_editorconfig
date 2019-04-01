971e55b1d7deedb9699fe1717f582c56766d0ea0
Alexey-t <support@uvviewsoft.com>
2019-04-01 20:42:07 +0300

    up

--- __init__.py
+++ __init__.py
@@ -13,13 +13,22 @@ class Command:
         fn = ed_self.get_filename()
         try:
             c = get_properties(fn)
-            if not c: return
-            self.apply(ed_self, c)
-            
+            if c:
+                self.apply(ed_self, c)
         except EditorConfigError:
-            s = 'Error getting EditorConfig props: '+fn
-            print(s)
+            s = 'Error getting EditorConfig properties'
             msg_status(s)
+            print(s+': '+fn)
             
     def apply(self, ed, c):
         print('EditorConfig:', c)
+        
+        opt = c.get('indent_style')
+        if opt=='space':
+            ed.set_prop(PROP_TAB_SPACES, True)
+        elif opt=='tab':
+            ed.set_prop(PROP_TAB_SPACES, False)
+        
+        opt = c.get('indent_size')
+        if opt:
+            ed.set_prop(PROP_TAB_SIZE, int(opt))
